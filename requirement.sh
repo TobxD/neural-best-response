f15e6c2a8624f3df98c70982c27b262edf471413
patrick <yihanz3@andrew.cmu.edu>
2023-11-04 21:49:01 -0400

    fixed something

--- requirement.sh
+++ requirement.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 conda create -n nbr python==3.11
-conda activate nbr
+source activate nbr
 pip install -U scikit-learn
 pip install pandas
-pip install --upgrade torchio
 pip install pyyaml
 pip install matplotlib
 pip install nashpy
 pip install open_spiel
+pip install nfn
+
+
+# run bash requirements.sh
