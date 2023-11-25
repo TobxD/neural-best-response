1bd21cda221497175cfa59d0d9e5dc83cacf40e2
yihanz3 <yihanz3@andrew.cmu.edu>
2023-11-25 14:56:03 -0500

    small modify

--- plot_traj.py
+++ plot_traj.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def extract_values(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -15,11 +17,24 @@ def extract_values(filename):
 
     return episode_values
 
-def get_traj_values(filename= 'trajectory/evaluation_log_20231125_132152.txt'):
+def plot_traj_values(filename):
     values = extract_values(filename)
-    for episode, vals in values.items():
-        print(f"Episode {episode}: {vals}")
-    return values
+    episodes = list(values.keys())
+    value_1 = [v[0][0] for v in values.values()]  #
+    value_2 = [v[1][0] for v in values.values()]  #
+    value_3 = [v[2][0] for v in values.values()]  #
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(episodes, value_1, label='Value of Table BR Policy')
+    plt.plot(episodes, value_2, label='Value of Hypernet BR Policy (1st)')
+    plt.plot(episodes, value_3, label='Value of Hypernet BR Policy (2nd)')
+    plt.xlabel('Episode')
+    plt.ylabel('Value')
+    plt.title('Values vs. Episodes')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 if __name__=='__main__':
-    get_traj_values()
+    file_time = '20231125_132152'
+    plot_traj_values(f'trajectory/evaluation_log_{file_time}.txt')
