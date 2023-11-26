4c4d55117c9db7e08e26f7a8ea0b27e73cc95a77
yihanz3 <yihanz3@andrew.cmu.edu>
2023-11-26 14:06:08 -0500

    plots for 4 traj

--- plot_traj.py
+++ plot_traj.py
@@ -22,31 +22,79 @@ def smooth_list(values, window_size):
     return smoothed_values
 
 
-def plot_traj_values(filename):
+def plot_traj_values(filename, base, time, name, title):
     values = extract_values(filename)
     episodes = list(values.keys())
     value_1 = [float(v[0]) for v in values.values()]  
     value_2 = [float(v[1]) for v in values.values()]  
     value_3 = [float(v[2]) for v in values.values()]
 
-
-
-
-
     plt.figure(figsize=(10, 6))
-    # plt.plot(episodes, value_1, label='Value of Table BR Policy')
-    plt.plot(episodes, value_2, label='Value of Hypernet BR Policy (1st)')
-    plt.plot(episodes, value_3, label='Value of Hypernet BR Policy (2nd)')
-    plt.axhline(y=value_1[0], linestyle='--')  
-    plt.ylim(0, 0.6)
-    plt.yticks([i/10.0 for i in range(11)])
+    # plt.plot(episodes, value_1, label='Expected Value')
+    plt.plot(episodes, value_2, label='Value of Hypernet BR Policy (softmax temp=1)', c = 'orange')
+    plt.plot(episodes, value_3, label='Value of Hypernet BR Policy (softmax temp=0)', c = 'green')
+    plt.axhline(y=value_1[0], linestyle='--', c = 'grey', label='theoretical maximum')
+    plt.axhline(base, linestyle='--', c='red', label='fixed network baseline')  
+    plt.ylim(0, 0.7)
+    plt.xlim(0, 100000)
     plt.xlabel('Episode')
     plt.ylabel('Value')
-    plt.title('Values vs. Episodes')
+    plt.title(title)
     plt.legend()
-    # plt.grid(True)
-    plt.show()
+    plt.savefig(f'trajectory/20231126_{time}/{name}.png', dpi=300)
+    plt.close()
 
 if __name__=='__main__':
-    file_time = '20231126_090030'
-    plot_traj_values(f'trajectory/evaluation_log_{file_time}.txt')
+    base_train_p0 = 0.42634892660048623
+    base_eval_p0 = 0.4223601409249119
+    base_train_p1 = 0.510115206566238
+    base_eval_p1 = 0.47288730269034057
+    date = '20231126'
+    time = '1127'
+    plot_traj_values(f'trajectory/20231126_{time}/train_evaluation_log.txt', base_train_p0, time, '0_train', 'training trajectory for player 0')
+    plot_traj_values(f'trajectory/20231126_{time}/eval_evaluation_log.txt', base_eval_p0, time, '0_eval', 'testing trajectory for player 0')
+
+    time = '1206'
+    plot_traj_values(f'trajectory/20231126_{time}/train_evaluation_log.txt', base_train_p1,time,'1_train', 'training trajectory for player 1')
+    plot_traj_values(f'trajectory/20231126_{time}/eval_evaluation_log.txt', base_eval_p1,time,'1_eval', 'testing trajectory for player 1')
+
+    time = '1218'
+    plot_traj_values(f'trajectory/20231126_{time}/train_evaluation_log.txt', base_train_p0,time,'0_train', 'training trajectory for player 0')
+    plot_traj_values(f'trajectory/20231126_{time}/eval_evaluation_log.txt', base_eval_p0,time,'0_eval', 'testing trajectory for player 0')
+
+    time = '1220'
+    plot_traj_values(f'trajectory/20231126_{time}/train_evaluation_log.txt', base_train_p1,time,'1_train', 'training trajectory for player 1')
+    plot_traj_values(f'trajectory/20231126_{time}/eval_evaluation_log.txt', base_eval_p1,time,'1_eval', 'testing trajectory for player 1')
+
+
+
+
+
+
+    """
+    =============================
+    # 1127:
+    nn_player 0
+    [128,128,128,128,128,128,128]
+    states + weights
+    =============================
+    # 1206:
+    nn_player 1
+    [128,128,128,128,128,128,128]
+    states + weights
+    =============================
+
+    
+    ---------------------TODO----------------------------
+    # 1218:
+    nn_player 0
+    [128,128,128,128,128,128,128]
+    weights
+
+    # 1220:
+    nn_player 1
+    [128,128,128,128,128,128,128]
+    weights
+
+    """
+    
